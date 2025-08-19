98cff2a7a3a2538745dd85dbe2ed0cfa59e25a37
eddizak <zakaria.eddini@divtec.ch>
2025-08-19 16:18:23 +0200

    Ajout du java script

---
+++ script.js
@@ -0,0 +1,31 @@
+/*
+* @author Zakaria Eddini
+* @version 0.1
+* @since 19.08.2025
+* */
+
+"use strict";
+
+let compteur = 0;
+const compteurEl = document.getElementById("compteur-el");
+const sauvegardeEl = document.getElementById("sauvegarde-el");
+
+function capturer(){
+    compteur+= 5;
+    compteurEl.textContent = compteur;
+
+    if(compteur < 5){
+        compteurEl.style.color = "red";
+    } else if(compteur < 20){
+        compteurEl.style.color = "green";
+    } else if(compteur > 30){
+        compteurEl.style.color = "yellow";
+    }
+}
+
+function sauvegarder() {
+    let compteurStr = "<li>" + compteur + " Pokémons</li>";
+    sauvegardeEl.innerHTML += compteurStr; // Ajouter la valeur actuelle du compteur
+    compteur = 0;
+    compteurEl.textContent = compteur;
+}
