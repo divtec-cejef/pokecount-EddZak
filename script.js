b43c38c1e5c0d9a4cd295f6267e9666463b45066
eddizak <zakaria.eddini@divtec.ch>
2025-08-19 16:22:31 +0200

    Ajout du java script

--- script.js
+++ script.js
@@ -9,6 +9,11 @@
 let compteur = 0;
 const compteurEl = document.getElementById("compteur-el");
 const sauvegardeEl = document.getElementById("sauvegarde-el");
+const capturerBtn = document.getElementById("capturer-btn");
+const sauvegarderBtn = document.getElementById("sauvegarder-btn");
+capturerBtn.addEventListener("click", capturer);
+sauvegarderBtn.addEventListener("click", sauvegarder);
+capturerBtn.addEventListener("click", capturer);
 
 function capturer(){
     compteur+= 5;
@@ -27,5 +32,8 @@ function sauvegarder() {
     let compteurStr = "<li>" + compteur + " Pokémons</li>";
     sauvegardeEl.innerHTML += compteurStr; // Ajouter la valeur actuelle du compteur
     compteur = 0;
+    localStorage.setItem("captures", sauvegardeEl.innerHTML);
     compteurEl.textContent = compteur;
 }
+window.addEventListener("load", () => {
+    sauvegardeEl.innerHTML = localStorage.getItem("captures") || "";});
